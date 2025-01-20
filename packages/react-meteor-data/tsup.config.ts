929f0d2cf6394107be31aa2884a6af3713099a51
Jørgen Vatle <jorgen@wcaservices.net>
2025-01-20 22:33:54 +0200

    Add suspense to build entries

--- packages/react-meteor-data/tsup.config.ts
+++ packages/react-meteor-data/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['index.js'],
+    entry: ['index.js', "suspense/index.js"],
     outDir: 'dist',
     format: ['esm', 'cjs'],
     external: ['meteor/meteor', 'meteor/mongo', 'meteor/tracker']
